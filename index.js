5e1622e148e748244bc45d08158e451cd9685652
masud467 <masudpfrnt@gmail.com>
2025-01-10 23:25:38 +0600

    add middleware.

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ require("dotenv").config();
 const port = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "https://ena-ema-task-for-client.vercel.app"
+
+    
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uoysey8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
